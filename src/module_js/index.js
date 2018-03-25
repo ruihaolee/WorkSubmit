37864ed3248459b63146cdf4e12d29b8f7b4586c
xshthebat <xushihao@xiyou3g.com>
2018-03-25 19:50:50 +0800

    text

--- src/module_js/index.js
+++ src/module_js/index.js
@@ -7,7 +7,7 @@ console.log(123);
 let name = "Bob", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
 
-
+console.log('haha');
 class Point {
   constructor(x, y) {
     this.x = x;

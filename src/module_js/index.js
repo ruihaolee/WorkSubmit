f4162d6ea07a74be7d241e11e206bbb9e482b911
xshthebat <xushihao@xiyou3g.com>
2018-03-25 21:56:56 +0800

    xshtext

--- src/module_js/index.js
+++ src/module_js/index.js
@@ -3,11 +3,9 @@ import './student.js';
 import Router from '../pub_funcs/router.js';
 var abc = 123;
 console.log(123);
-
 let name = "Bob", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
-
-console.log('haha');
+console.log('xshtext');
 class Point {
   constructor(x, y) {
     this.x = x;
